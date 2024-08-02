e31d79d17ff05486ec47471839a8224236660bf1
facchin21 <130471266+facchin21@users.noreply.github.com>
2024-08-02 14:34:06 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,21 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
 
     return (
-      <nav className='bg-black w-full px-4 md:px-12 flex flex-col md:flex-row justify-between items-center'>
-        <div className='flex flex-row items-center justify-center mb-4 md:mb-0 pb-4 md:pb-0 w-full'>
-            <img src={imageRiot} alt="image logo Riot" className='w-full md:w-auto h-auto md:h-12' />
-            <img src={imageLogo} alt="Image logo Valorant" className='w-full md:w-auto h-auto md:h-12 ml-2' />
-        </div>
-        <ul className='font-Farro flex flex-col md:flex-row gap-2 items-center justify-center'>
-            {items.map(item => (
-                <li key={item.id}>
-                    <Link to={`/${item.link}`} 
-                    className='text-white transition-all duration-300 hover:text-red-500'>
-                        {item.name}
-                    </Link>
-                </li>
-            ))}
-        </ul>
+     <nav className='bg-black w-full px-4 md:px-12 flex flex-col md:flex-row justify-between items-center'>
+            <div className='flex flex-row items-center justify-center mb-4 md:mb-0 pb-4 md:pb-0 w-full'>
+                <img src={imageRiot} alt="image logo Riot" className='md:w-full h-auto md:h-12' />
+                <img src={imageLogo} alt="Image logo Valorant" className='md:w-full h-auto md:h-12 ml-2' />
+            </div>
+            <ul className='font-Farro flex flex-col pb-6 md:flex-row gap-2 items-center justify-center'>
+                {items.map(item => (
+                    <li key={item.id}>
+                        <Link to={`/${item.link}`} 
+                        className='text-white transition-all duration-300 hover:text-red-500'>
+                            {item.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
     </nav>
 
     )
